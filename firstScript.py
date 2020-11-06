b7bbe900e7040a89c6d82b31cd2e76866ca258f2
AshleyCahalan <a_cahalan@fanshaweonline.ca>
2020-11-05 19:56:08 -0500

    adding python file

---
+++ firstScript.py
@@ -0,0 +1,23 @@
+# This is a python comment
+
+# print is a built-in Python function that outputs a message to a screen (terminal window, UI, etc)
+print("hello from Python!")
+
+# variables - pieces of data with values that change
+
+score = 0
+
+print("score is: " + str(score))
+
+score = 150000
+
+print("score is: " + str(score))
+
+temperature = 25
+
+if (temperature < 25):
+	print("maybe wear a jacket")
+elif (temperature > 25):
+	print("it's warm! sun's out guns out!")
+else:
+	print("perfect summer day")
